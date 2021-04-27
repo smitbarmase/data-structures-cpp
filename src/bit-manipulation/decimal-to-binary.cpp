b7a3ad6d2aec6a9d915192e2c3a258791711332d
Smit Barmase <smitbarmase@outlook.com>
2021-04-28 02:11:44 +0530

    decimal to binary

---
+++ src/bit-manipulation/decimal-to-binary.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pq priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+
+int decimal_to_binary(int n)
+{
+    int ans = 0, p = 1;
+    while (n > 0)
+    {
+        ans += (n & 1) * p;
+        p *= 10;
+        n = n >> 1;
+    }
+    return ans;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << decimal_to_binary(n) << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

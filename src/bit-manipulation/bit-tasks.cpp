5b7da191ae55097ec9e3c830ad9c8fe57b8b4419
Smit Barmase <smitbarmase@outlook.com>
2021-04-28 00:26:14 +0530

    clear last i bits and clear range i to j

--- src/bit-manipulation/bit-tasks.cpp
+++ src/bit-manipulation/bit-tasks.cpp
@@ -34,11 +34,25 @@ int update_bit(int n, int i, int v)
     return (n & ~(1 << i)) | (v << i);
 }
 
+int clear_last_i_bits(int n, int i)
+{
+    return n & (~0 << i);
+}
+
+int clear_range_i_to_j(int n, int i, int j)
+{
+    return n & ((~0 << (j + 1)) | ((1 << i) - 1));
+}
+
 void solve()
 {
     cout << update_bit(13, 1, 1) << endl;
     // 1 1 0 1 = 13 => input
     // 1 1 1 1 = 15 => output
+
+    cout << clear_range_i_to_j(31, 1, 3) << endl;
+    // 0 1 1 1 1 1 = 31 => input
+    // 0 1 0 0 0 1 = 17 => output
 }
 
 int32_t main()

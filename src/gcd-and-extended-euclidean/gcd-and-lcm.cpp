e58c24e975775c1b309ee72a8b04a7f36896e157
Smit Barmase <smitbarmase@outlook.com>
2021-05-02 01:23:51 +0530

    gcd-and-lcm

---
+++ src/gcd-and-extended-euclidean/gcd-and-lcm.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pq priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+
+// Find GCD and LCM of a, b
+
+int get_gcd(int a, int b)
+{
+    return b == 0 ? a : get_gcd(b, a % b);
+}
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+
+    int gcd = get_gcd(a, b);
+
+    int lcm = a * b / gcd;
+
+    cout << gcd << " " << lcm << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

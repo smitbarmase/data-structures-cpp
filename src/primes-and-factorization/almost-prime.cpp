7cb0719ac2783a63b6bc851dca436782b99caf0e
Smit Barmase <smitbarmase@outlook.com>
2021-05-01 14:49:47 +0530

    almost-prime

---
+++ src/primes-and-factorization/almost-prime.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pq priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+
+// Problem
+// https://codeforces.com/problemset/problem/26/A
+
+// Input
+// 2
+// 10
+// 21
+
+// Output
+// 2
+// 8
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int arr[n + 1] = {0}; // it will count divisors
+
+    for (int i = 2; i <= n; i++)
+    {
+        if (arr[i] == 0)
+        {
+            for (int j = i; j <= n; j += i)
+            {
+                arr[j]++;
+            }
+        }
+    }
+
+    int count = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        if (arr[i] == 2)
+        {
+            count++;
+        }
+    }
+
+    cout << count << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

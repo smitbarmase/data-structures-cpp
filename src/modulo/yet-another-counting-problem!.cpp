9927042cf6aa8e1569205022bb88ba3b4877e10f
Smit Barmase <smitbarmase@outlook.com>
2021-05-02 18:22:36 +0530

    yet-another-counting-problem

---
+++ src/modulo/yet-another-counting-problem!.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pq priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+
+// https://codeforces.com/problemset/problem/1342/C
+
+// Input:
+// 2
+// 4 6 5
+// 1 1
+// 1 3
+// 1 5
+// 1 7
+// 1 9
+// 7 10 2
+// 7 8
+// 100 200
+
+// Output:
+// 0 0 0 2 4
+// 0 91
+
+void solve()
+{
+    int a, b, q;
+    cin >> a >> b >> q;
+
+    while (q--)
+    {
+        int l, r;
+        cin >> l >> r;
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

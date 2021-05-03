c3f8f345ea3e3fc18d5433a6f425daa250528ced
Smit Barmase <smitbarmase@outlook.com>
2021-05-03 13:05:59 +0530

    first and last occurence

---
+++ src/recursion-basics/last-occurence.cpp
@@ -0,0 +1,73 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pq priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+
+int last_occurence(int *arr, int n, int k)
+{
+    // base case
+    if (n == 0)
+    {
+        return -1;
+    }
+
+    // rec case
+    int i = last_occurence(arr + 1, n - 1, k);
+    if (i == -1)
+    {
+        if (arr[0] == k)
+        {
+            return 0;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    return i + 1;
+}
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << last_occurence(arr, n, k) << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

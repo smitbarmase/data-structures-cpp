937fdbed02245864ec0bba46d602da192220ae10
Smit Barmase <smitbarmase@outlook.com>
2021-05-04 22:42:55 +0530

    optimal-binary-strings

---
+++ src/recursion-quick-thinking/optimal-binary-strings.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pq priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+
+int optimal_binary_strings(int n)
+{
+    // Base case
+    if (n == 0)
+    {
+        return 1;
+    }
+    if (n == 1)
+    {
+        return 2;
+    }
+
+    // Recursive case
+    return optimal_binary_strings(n - 1) + optimal_binary_strings(n - 2);
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    cout << optimal_binary_strings(n) << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}

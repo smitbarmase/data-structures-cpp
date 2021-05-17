b6c0a054b4757888d8b594badbb55332b13977af
Smit Barmase <smitbarmase@outlook.com>
2021-05-17 05:51:50 +0530

    cycle-detection

---
+++ src/graph-undirected/cycle-detection.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+#define mii map<int, int>
+#define pq priority_queue<int>
+#define pqs priority_queue<int, vi, greater<int>>
+#define setbits(x) __builtin_popcountll(x)
+#define zerobits(x) __builtin_ctzll(x)
+#define mod 1000000007
+#define inf 1e18
+
+template <typename T>
+class Graph
+{
+    int n;
+    map<T, list<T>> l;
+
+public:
+    Graph(int n)
+    {
+        this->n = n;
+    }
+
+    void add_edge(T a, T b)
+    {
+        l[a].push_back(b);
+        l[b].push_back(a);
+    }
+
+    bool cycle_helper(T node, T parent, bool *visited)
+    {
+        visited[node] = 1;
+        for (auto child : l[node])
+        {
+            if (!visited[child])
+            {
+                bool cycle = cycle_helper(child, node, visited);
+                if (cycle)
+                {
+                    return true;
+                }
+            }
+            else if (child != parent)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool have_cycle()
+    {
+        bool *visited = new bool[n]{0};
+        return cycle_helper(0, -1, visited);
+    }
+};
+
+void solve()
+{
+    Graph<int> g(5);
+    g.add_edge(0, 1);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    // g.add_edge(3, 0);
+    g.add_edge(3, 4);
+    g.add_edge(3, 5);
+    cout << g.have_cycle() << endl;
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
